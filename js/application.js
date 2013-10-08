100b80a5fce03782c93ffaadc8ddf1ad485553a0
song940 <admin@lsong.org>
2013-10-08 17:26:31 +0800

    change my life .

--- js/application.js
+++ js/application.js
@@ -29,5 +29,45 @@
             });
         
         });
+
+        $.get('https://api.github.com/users/song940/repos',function(data){
+            $.each(data,function(i,item){
+                var proj = $('<div />').addClass('span4');
+                $('<i>').addClass('icon icon-github').appendTo(proj);
+                $('<a>').text(item.name)
+                    .attr('href',item.html_url)
+                    .appendTo(proj);
+                $('<p>').text(item.description).appendTo(proj);
+
+                $('#projects-list').append(proj);
+            });
+        });
+
+
+        $('a').smoothScroll({
+          offset: 0,
+
+          // one of 'top' or 'left'
+          direction: 'top',
+
+          // only use if you want to override default behavior
+          scrollTarget: null,
+
+          // fn(opts) function to be called before scrolling occurs.
+          // `this` is the element(s) being scrolled
+          beforeScroll: function() {},
+
+          // fn(opts) function to be called after scrolling occurs.
+          // `this` is the triggering element
+          afterScroll: function() {},
+          easing: 'swing',
+          speed: 400,
+
+          // coefficient for "auto" speed
+          autoCoefficent: 2
+
+        });
+
+        $('#navbar').scrollspy();
     });
 })(window,jQuery);
